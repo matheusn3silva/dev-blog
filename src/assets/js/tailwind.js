6dd7c3c179884c56af394ada337dafb8a405de05
Matheus Nascimento da Silva <matheusnascimentox725@gmail.com>
2024-02-12 16:33:55 -0300

    header mobile

---
+++ src/assets/js/tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        main: '#846AFF',
+        dark: '#121214',
+        light: '#F8F8FF',
+        gray: '#7C7C8A'
+      }
+    }
+  }
+}
