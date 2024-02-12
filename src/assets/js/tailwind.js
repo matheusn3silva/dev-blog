ab4baa434304fa7fec0bdf8ee328db32c73427e6
Matheus Nascimento da Silva <matheusnascimentox725@gmail.com>
2024-02-12 19:14:19 -0300

    Footer contact complete

--- src/assets/js/tailwind.js
+++ src/assets/js/tailwind.js
@@ -2,11 +2,12 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        main: '#846AFF',
-        dark: '#121214',
-        light: '#F8F8FF',
-        gray: '#7C7C8A'
-      }
-    }
-  }
-}
+        main: "#846AFF",
+        dark: "#121214",
+        light: "#F8F8FF",
+        gray: "#7C7C8A",
+        lightgray: "#EEEEFF",
+      },
+    },
+  },
+};
