8fe6faebe9cc33cbd976f41d71df0eee728858d3
Matheus Nascimento da Silva <matheusnascimentox725@gmail.com>
2024-02-13 11:26:46 -0300

    Responsive and ScrollReveal add

---
+++ src/assets/js/scrollreveal.js
@@ -0,0 +1,90 @@
+window.animate = ScrollReveal({ reset: true });
+
+// HEADER
+
+animate.reveal(".txt-scroll", {
+  duration: 2000,
+  distance: "120px",
+  origin: "left",
+});
+
+animate.reveal(".img-scroll", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "right",
+});
+
+// MAIN
+
+animate.reveal('.title-scroll', {
+  duration: 2000,
+  distance: '30px',
+  origin: 'left'
+});
+
+animate.reveal(".article1-scroll", {
+  duration: 1500,
+  scale: 0.5,
+});
+
+animate.reveal(".article2-scroll", {
+  duration: 1500,
+  scale: 0.5,
+  delay: 250,
+});
+
+animate.reveal(".article3-scroll", {
+  duration: 1500,
+  scale: 0.5,
+  delay: 500,
+});
+
+animate.reveal(".article4-scroll", {
+  duration: 1500,
+  scale: 0.5,
+  delay: 750,
+});
+
+animate.reveal(".article5-scroll", {
+  duration: 1500,
+  scale: 0.5,
+  delay: 1000,
+});
+
+animate.reveal(".article6-scroll", {
+  duration: 1500,
+  scale: 0.5,
+  delay: 1250,
+});
+
+// FOOTER
+
+animate.reveal(".name-scroll", {
+  duration: 2000,
+  distance: "50px",
+  origin: "top",
+});
+
+animate.reveal(".email-scroll", {
+  duration: 2000,
+  distance: "50px",
+  delay: 500,
+  origin: "top",
+});
+
+animate.reveal(".area-scroll", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1000,
+  origin: "top",
+});
+
+animate.reveal(".contactbtn-scroll", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1000,
+  origin: "bottom",
+});
+
+
