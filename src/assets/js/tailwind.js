4a852da463a91645f66a2a749c483a6bde4d582f
Matheus Nasc <matheusnascimentox725@gmail.com>
2024-02-13 12:26:55 -0300

    Alterations

--- src/assets/js/tailwind.js
+++ src/assets/js/tailwind.js
@@ -4,9 +4,8 @@ tailwind.config = {
       colors: {
         main: "#846AFF",
         dark: "#121214",
-        light: "#F8F8FF",
         gray: "#7C7C8A",
-        lightgray: "#EEEEFF",
+        lightpurple: "#EEEEFF",
       },
     },
   },
